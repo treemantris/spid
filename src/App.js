bc0f0ffb00da439dd4f6314df8d35e1c324e406f
treemantris <treemantris@users.noreply.github.com>
2018-04-08 15:23:31 +0100

    Basic slider working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Thermostat from 'react-nest-thermostat'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css';
 
 class App extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {target: 70}
+  }
+
   render() {
+      var targetTemperature = this.state.target
+      var currentTemperature = 60
+      var updateTargetTemperature = (newValue) => this.setState({target:newValue})
+      var props = {currentTemperature, targetTemperature}
+      var props2 = {targetTemperature, updateTargetTemperature}
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TemperatureDisplay {...props}/>
+        <TemperatureSlider {...props2}/>
       </div>
     );
   }
+}
+
+class TemperatureDisplay extends Component {
+    render() {
+        var{currentTemperature, targetTemperature} = this.props
+        var hvacMode
+        if (targetTemperature > currentTemperature)
+            hvacMode = "heating"
+        else if (targetTemperature < currentTemperature) 
+            hvacMode = "cooling"
+        else
+            hvacMode = "off"
+        var newProps =  {ambientTemperature: currentTemperature, targetTemperature, hvacMode}
+        return (
+            <div>
+                <Thermostat {...newProps}/>
+            </div>
+        )
+    }
+}
+
+class TemperatureSlider extends Component {
+    render() {
+        var {targetTemperature, updateTargetTemperature} = this.props
+        return (
+            <div>
+                <Slider defaultValue={targetTemperature} onChange={updateTargetTemperature}/>
+            </div>
+        )
+    }
 }
 
 export default App;
